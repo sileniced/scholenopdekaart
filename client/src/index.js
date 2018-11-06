bd33209f948e7159a6821a13778a95278e9842e4
Gregorio Ferrazin <gregorio.ferrazin@gmail.com>
2018-11-06 16:57:05 +0100

    search bar start

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import { Provider } from "react-redux";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
+// import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+
+import {theme} from "./theme";
+import App from "./App";
 import store from "./store";
+
+
 
 ReactDOM.render(
   <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
