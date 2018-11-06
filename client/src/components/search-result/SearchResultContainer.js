79e2f44d2e91c46005705ed0d30f4f549d134e69
VosKoen <Koen82@gmail.com>
2018-11-06 11:19:18 +0100

    basic search result container

---
+++ client/src/components/search-result/SearchResultContainer.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+class SearchResultContainer extends React.PureComponent {
+  render() {
+    return <div />;
+  }
+}
+
+const mapStateToProps = state => ({});
+
+export default connect(mapStateToProps)(EventDisplayContainer);
