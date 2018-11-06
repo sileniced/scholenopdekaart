198f7aad49ce50eef193fc0ad6f39231edb7f141
Vince Liem <vincepaul.liem@gmail.com>
2018-11-06 18:52:09 +0100

    the backend is finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,18 @@
 import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
-import SchoolController from "./schools/controller";
-import setupDb from './db'
 
-const port = process.env.PORT || 4000
+import * as Koa from 'koa'
+import { useKoaServer } from 'routing-controllers'
 
-const app = createKoaServer({
-   controllers: [SchoolController]
+import AppController from './AppController'
+
+const app = new Koa()
+const port = 4000
+
+useKoaServer(app, {
+  cors: true,
+  controllers: [
+    AppController
+  ]
 })
 
-
-setupDb()
-  .then(_ =>
-    app.listen(port, () => console.log(`Listening on port ${port}`))
-  )
-  .catch(err => console.error(err))
+app.listen(port, () => console.log(`Listening on port ${port}`))
