ae44773a691f2e5a6e5a4eedd98db54aa09b48a2
VosKoen <Koen82@gmail.com>
2018-11-09 15:54:59 +0100

    some changes

--- client/src/components/filter/FilterSelectionContainer.js
+++ client/src/components/filter/FilterSelectionContainer.js
@@ -3,22 +3,43 @@ import { connect } from "react-redux";
 import FilterSelection from "./FilterSelection";
 import { filters } from "../../constants";
 import { toggleFilter, resetFilters } from "../../actions/filters";
-import FilterSelectionButtons from './FilterSelectionButtons';
+import FilterSelectionButtons from "./FilterSelectionButtons";
+
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import TuneIcon from 'mdi-react/TuneIcon'; 
+
 
 class FilterSelectionContainer extends React.PureComponent {
   handleToggle = filter => {
-    this.props.toggleFilter(filter)
+    this.props.toggleFilter(filter);
   };
 
   handleResetFilters = () => {
-      this.props.resetFilters();
-  }
-    
+    this.props.resetFilters();
+  };
+
   render() {
     return (
       <div>
-        <FilterSelection filters={filters} handleToggle={this.handleToggle} selectedFilters={this.props.selectedFilters}/>
-        <FilterSelectionButtons handleResetFilters={this.handleResetFilters}/>
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={ <TuneIcon />}>
+          Vergelijk scholen op...
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <div>
+              <FilterSelection
+                filters={filters}
+                handleToggle={this.handleToggle}
+                selectedFilters={this.props.selectedFilters}
+              />
+              <FilterSelectionButtons
+                handleResetFilters={this.handleResetFilters}
+              />
+            </div>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </div>
     );
   }
@@ -31,5 +52,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {toggleFilter, resetFilters}
+  { toggleFilter, resetFilters }
 )(FilterSelectionContainer);
