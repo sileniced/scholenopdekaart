397f8b973e26f78802a038e80c98680ed93d5e0a
Vince Liem <vincepaul.liem@gmail.com>
2018-11-09 13:50:24 +0100

    Merge branch 'master' of https://bitbucket.org/VosKoen/back-2-school into charts
    # Conflicts:
    #	client/src/App.js

--- client/src/components/charts/Tijden.js
+++ client/src/components/charts/Tijden.js
@@ -1,47 +1,82 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
 
 import { checkPoindAvailability } from '../../utilities'
 
-import Highcharts from 'highcharts'
+import Highcharts from 'highcharts/'
 import HighchartsReact from 'highcharts-react-official'
+import xrange from 'highcharts/modules/xrange-series'
+
+xrange(Highcharts)
+
+const categories = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag']
+const parts = ['opvangVan', 'ochtendVan', 'ochtendTot', 'middagVan', 'middagTot', 'opvangTot']
+const parseTime = time => moment(time, 'HH:mm').add(1, 'hour').valueOf()
+
+const inputMapped = input => input.map(day => {
+  const y = categories.indexOf(day['dag'])
+  let partialFill = 0
+
+  return parts.map((part, i) => {
+    partialFill = partialFill ? 0 : 1
+    for (let ii = i + 1; ii < parts.length; ii++) {
+      if (day[part] && parts[ii] && day[parts[ii]]) {
+        return {
+          x: parseTime(day[part]),
+          x2: parseTime(day[parts[ii]]),
+          y,
+          partialFill,
+        }
+      }
+    }
+    return false
+
+  }).filter(part => part)
+
+}).reduce((output, day) => output.concat(day), [])
 
 class Tijden extends Component {
-
-  // constructor(props) {
-  //   super(props)
-  //   this.props = props
-  //
-  // }
 
   render() {
 
     const { available, unavailable } = checkPoindAvailability(this.props.selectedSchools, 'tijden')
-    const schools = available.map(school => school.__TEMPLATE__)
 
-    const options = {
-      chart: {
-        // https://www.highcharts.com/docs/chart-and-series-types/chart-types
-        type: '__TEMPLATE__',
-      },
-      title: {
-        text: '__TEMPLATE__',
-      },
-      subtitle: {
-        text: '__TEMPLATE__',
-      },
-      xAxis: {
-        categories: ['__TEMPLATE__'],
-      },
-      series: [
-        {
-          name: '__TEMPLATE__',
-          data: ['__TEMPLATE__']
+    console.log(available[0] && inputMapped(available[0].rapport.versie1.datasetSchooltijden.rij))
+
+    const dataset = available.map(school => ({
+      C: school.C,
+      data: {
+        chart: {
+          type: 'xrange',
         },
-      ],
-    }
+        title: {
+          text: '',
+        },
+        xAxis: {
+          type: 'datetime',
+        },
+        yAxis: {
+          title: {
+            text: '',
+          },
+          categories,
+          reversed: true,
+        },
+        series: [
+          {
+            name: school.school.N,
+            data: inputMapped(school.rapport.versie1.datasetSchooltijden.rij),
+          },
+        ],
+      },
+    }))
 
-    return <HighchartsReact highcharts={Highcharts} options={options}/>
+    return (
+      <div>
+        {dataset.map(data => <HighchartsReact key={data.C} highcharts={Highcharts} options={data.options}/>)}
+      </div>
+    )
   }
 }
 
--- client/src/utilities.js
+++ client/src/utilities.js
@@ -1,16 +1,4 @@
-export const checkPoindAvailability = (schools, poind) => {
-
-  const init = {
-    available: [],
-    unavailable: [],
-  }
-
-  return schools.reduce((a, school) => {
-
-    if (school[poind].rapport) a.available = [...a.available, school]
-    else a.unavailable = [...a.unavailable, school]
-
-    return a;
-
-  }, init)
-}
+export const checkPoindAvailability = (schools, poind) => ({
+  available:    schools.filter(school => !!(school[poind] && school[poind].rapport)),
+  unavailable:  schools.filter(school => !(school[poind] && school[poind].rapport)),
+})
