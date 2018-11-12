443bfa093a0b48adce40085742cf7b402839218b
Vince Liem <vincepaul.liem@gmail.com>
2018-11-12 16:33:03 +0100

    started eindresultaat

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import SearchResultContainer from "./components/search-result/SearchResultContainer";
-import SearchBarContainer from "./components/search/SearchBarContainer";
-import FilterSelectionContainer from "./components/filter/FilterSelectionContainer";
-import HeaderContiner from './components/layout/HeaderContainer'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import SearchResultContainer from './components/search-result/SearchResultContainer';
+// import SearchBarContainer from './components/search/SearchBarContainer';
+// import FilterSelectionContainer from './components/filter/FilterSelectionContainer';
+// import HeaderContiner from './components/layout/HeaderContainer'
 import TopBar from './components/layout/TopBar'
+import Eindtoets from './components/charts/Eindtoets'
+// import AantalLeerlingen from './components/charts/AantalLeerlingen'
+// import Tijden from './components/charts/Tijden'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-        <TopBar />
-          <HeaderContiner />
-          <SearchResultContainer />
-          <FilterSelectionContainer />
+          <TopBar/>
+          {/*<AantalLeerlingen multi />*/}
+          {/*<Tijden/>*/}
+          <Eindtoets/>
+          {/*<HeaderContiner/>*/}
+          {/*<SearchResultContainer/>*/}
+          {/*<FilterSelectionContainer/>*/}
         </div>
       </Router>
     );
