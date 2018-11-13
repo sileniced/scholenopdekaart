c84a0a3562f09db571d0a8ce511a4a0be7031abe
Vince Liem <vincepaul.liem@gmail.com>
2018-11-13 15:19:58 +0100

    Finished EindToets

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import SearchResultContainer from "./components/search-result/SearchResultContainer";
-import SearchBarContainer from "./components/search/SearchBarContainer";
-import FilterSelectionContainer from "./components/filter/FilterSelectionContainer";
-import HeaderContainer from "./components/layout/HeaderContainer";
-import TopBar from "./components/layout/TopBar";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import SearchResultContainer from './components/search-result/SearchResultContainer'
+import FilterSelectionContainer from './components/filter/FilterSelectionContainer'
+import HeaderContainer from './components/layout/HeaderContainer'
+import TopBar from './components/layout/TopBar'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-            <header>
-              <TopBar />
-            </header>
-            <main>
-              <HeaderContainer />
-              <SearchResultContainer />
-              <FilterSelectionContainer />
-            </main>
+          <header>
+            <TopBar/>
+          </header>
+          <main>
+            <HeaderContainer/>
+            <SearchResultContainer/>
+            <FilterSelectionContainer/>
+          </main>
         </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
