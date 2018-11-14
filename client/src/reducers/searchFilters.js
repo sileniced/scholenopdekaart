9eca7ca9f3e781ba31b21eab7147c48415e6b123
VosKoen <Koen82@gmail.com>
2018-11-14 11:38:48 +0100

    first working filter on search!

---
+++ client/src/reducers/searchFilters.js
@@ -0,0 +1,30 @@
+import { TOGGLE_SEARCH_FILTER , SET_SEARCH_FILTERS_FALSE } from "../actions/searchFilters";
+import { typeOnderwijs, denominatie } from "../constants";
+
+const initialState = {typeOnderwijs: {},
+denominatie: {}};
+Object.keys(typeOnderwijs).map((type) => {initialState.typeOnderwijs[type] = false
+return type})
+Object.keys(denominatie).map((denominatie) => {initialState.denominatie[denominatie] = false
+    return denominatie})
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    case TOGGLE_SEARCH_FILTER:
+
+      const newSetting = !state[Object.keys(action.payload)][Object.values(action.payload)];
+      const returnObject = { ...state };
+      returnObject[Object.keys(action.payload)][Object.values(action.payload)] = newSetting;
+      return returnObject;
+
+    // case SET_SEARCH_FILTERS_FALSE:
+    //   const returnAllFalse = {...state};
+    //   for(let filter in returnAllFalse) {
+    //     returnAllFalse[filter] = false;
+    //   }
+    //   return returnAllFalse;
+      
+    default:
+      return state;
+  }
+};
