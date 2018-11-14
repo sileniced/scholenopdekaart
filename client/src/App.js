a1875a0128061b58231e31b6e3a7fdaa83c6ca55
VosKoen <Koen82@gmail.com>
2018-11-14 15:50:51 +0100

    some style changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,46 @@ import SearchResultContainer from "./components/search-result/SearchResultContai
 import FilterSelectionContainer from "./components/filter/FilterSelectionContainer";
 import HeaderContainer from "./components/layout/HeaderContainer";
 import TopBar from "./components/layout/TopBar";
-import ContactDetailsContainer from './components/contactDetails/ContactDetailsContainer'
+import ContactDetailsContainer from "./components/contactDetails/ContactDetailsContainer";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+ app: {
+   display:"flex",
+   flexDirection: "column",
+   alignItems:"center"
+ }
+});
 
 class App extends Component {
+
   render() {
+    const { classes } = this.props;
     return (
       <Router>
-        <div className="App">
+        <div className={classes.app}>
           <header>
-            <TopBar/>
+            <TopBar />
           </header>
           <main>
-            <HeaderContainer/>
-            <Route exact path="/search-result" component={SearchResultContainer}/>
-            <Route exact path="/search-result/:city" component={SearchResultContainer}/>
-            <FilterSelectionContainer/>
+            <HeaderContainer />
+            <Route
+              exact
+              path="/search-result"
+              component={SearchResultContainer}
+            />
+            <Route
+              exact
+              path="/search-result/:city"
+              component={SearchResultContainer}
+            />
+            <FilterSelectionContainer />
             <ContactDetailsContainer />
           </main>
         </div>
       </Router>
-    )
+    );
   }
 }
 
-export default App
+export default withStyles(styles)(App);
--- client/src/components/search-result/CompareButton.js
+++ client/src/components/search-result/CompareButton.js
@@ -2,32 +2,54 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
 const styles = theme => ({
   button: {
     margin: theme.spacing.unit,
+    padding: "5px",
     borderRadius: 0,
     boxShadow: "none",
+    color: "white",
+
   },
-  input: {
-    display: "none"
+  buttonText: {
+    color: "white",
+    fontSize: "0.9em",
+    margin: "5px"
+  },
+  buttonContent: {
+    display: "flex",
+    justifyContent: "space-between"
   }
+
 });
 
 function CompareButton(props) {
   const { classes } = props;
   return (
     <div>
-      <Button variant="contained" color={(props.school.selected) ? "secondary" : "primary"} className={classes.button} onClick={() => props.handleClick(props.schoolId)} disabled={props.selectedSchools.length < props.maxSchools || props.school.selected ? false : true}>
-        <Grid container spacing={24}>
-          <Grid item xs>
-            {(props.school.selected)? <span>&#10003;</span> : <span>&#43;</span>}
-          </Grid>
-          <Grid item xs>
-            Vergelijk
-          </Grid>
-        </Grid>
+      <Button
+        variant="contained"
+        color={props.school.selected ? "secondary" : "primary"}
+        className={classes.button}
+        onClick={() => props.handleClick(props.schoolId)}
+        disabled={
+          props.selectedSchools.length < props.maxSchools ||
+          props.school.selected
+            ? false
+            : true
+        }
+      ><div className={classes.buttonContent}>
+      <div>
+        <Typography className={classes.buttonText}>
+          {props.school.selected ? <span>&#10003;</span> : <span>&#43;</span>}
+        </Typography>
+        </div>
+        <div>
+        <Typography className={classes.buttonText}>Vergelijk</Typography>
+        </div>
+        </div>
       </Button>
     </div>
   );
